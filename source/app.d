9a813fa7e2f56273eb0e36c851ef2c8fc0f512a7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-02-25 02:35:24 -0500

    Ditto

--- source/app.d
+++ source/app.d
@@ -1,16 +1,20 @@
 import std.stdio;
 import window.window;
+import mesh.mesh;
 
 void main()
 {
     // Window controls OpenGL and GLFW
 	Window window = new Window("easygltf prototyping").initialize;
 
+    //* Allow direct message passing through reference pointers. Reduces verbosity.
+    Mesh.assignWindowContext(window);
+
 
     while (!window.shouldClose()) {
         window.clear(1,1,1);
 
-        
+
 
         window.swapBuffers();
         window.pollEvents();
