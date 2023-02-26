edba4dfde9451738b15119ad5e9e21b67fd6604c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-02-26 16:33:38 -0500

    Framework

--- source/easygltf/easygltf.d
+++ source/easygltf/easygltf.d
@@ -1,16 +1,30 @@
 module easygltf.easygltf;
 
+import std.stdio;
 import tinygltf;
 
+/// Stores all OpenGL raw data.
+class Mesh {
+
+}
+
+/// GLTF context to get OpenGL data. Automatically loads and decodes JSON data.
 class EasyGLTF {
 
     Model model;
 
     this(string fileLocation, bool debugInfo = false) {
+
+        // Automatically load and decode the JSON file. Store all data into model. Becomes usable data.
         this.model = new Model(fileLocation, debugInfo);
 
         if (!model.loadFile()) {
             throw new Exception("Failed to load model " ~ fileLocation ~ "!");
+        }
+
+        // Now automatically decode the decoded model. Turns it into raw OpenGL data that you can utilize easily.
+        foreach (mesh; model.meshes) {
+            writeln(mesh.name);
         }
     }
 
